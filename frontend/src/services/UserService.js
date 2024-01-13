35bb5416785c33900d30f40fd7620077808fe8af
Dustin9x <phamhuyhoang9x@gmail.com>
2024-01-13 10:20:22 +0700

    Update UserService.js

--- frontend/src/services/UserService.js
+++ frontend/src/services/UserService.js
@@ -7,16 +7,29 @@ export class UserService extends baseService {
     }
 
     login = (loginInfo) => {
-        return this.post(`api/auth/`,loginInfo);
+        return this.post(`api/auth/`, loginInfo);
     }
 
     register = (registerInfo) => {
-        return this.post(`api/auth/register`,registerInfo);
+        return this.post(`api/auth/register`, registerInfo);
     }
 
     layLaiMatKhau = (thongTinEmail) => {
         return this.post(`/api/auth/passwordRetrieval`, thongTinEmail);
-      };
+    };
+    
+    GetListUser = () => {
+        return this.get(`/User`);
+    };
+    createUser = (User) => {
+        return this.post(`/User`, User);
+    };
+    deleteUser = (id) => {
+        return this.delete(`/User?Id=${id}`);
+    };
+    updateUser = (id, User) => {
+        return this.put(`/User/${id}`, User);
+    };
 }
 
 export const userService = new UserService();
