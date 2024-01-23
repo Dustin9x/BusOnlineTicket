dcbcf9bb3acb4e100c7fd500007b6ac6065bd47f
phuslee24 <tranphu.thpthht@gmail.com>
2024-01-23 19:44:31 +0700

    update popularroute

--- frontend/src/components/PopularRoute/PopularRoute.js
+++ frontend/src/components/PopularRoute/PopularRoute.js
@@ -1,11 +1,10 @@
-import { Card, Tag } from "antd";
+import { Card, Carousel, Tag } from "antd";
 import React, { useEffect, useRef } from "react";
 import { SearchOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from "react-redux";
 import { getTripListAction } from "../../redux/actions/TripAction";
 import { DOMAIN } from "../../util/settings/config";
 import { getPromoteTripListAction } from "../../redux/actions/PromoteTripAction";
-
 
 export default function PopularRoute() {
     const { Meta } = Card;
@@ -15,25 +14,38 @@ export default function PopularRoute() {
         dispatch(getPromoteTripListAction())
     }, [])
 
+
+    const settings = {
+      autoplay:true,
+      autoplaySpeed:1500,
+      className: "center",
+      infinite: true,
+      slidesToShow: 5,
+      swipeToSlide: true,
+    }
+
     return (
         <div>
             <h1 className="text-center text-2xl">Popular bus routes</h1>
-            <div className="row p-4 rounded-xl bg-white" style={{height: 330}}>
-                {arrPromoteTrip?.map((item, index) => {
-                    return (
-                        <a href={`/search/?from=${item.fromStation}&to=${item.toStation}`} key={index} className="hover:no-underline col-2">
-                            <Card
-                                hoverable
-                                style={{ width: '100%' }}
-                                cover={<img alt="example" style={{ height: 150, objectFit: 'cover' }} src={`${DOMAIN}/Images/PromoteTrip/${item.image}`} />}
-                            >
-                                <Meta style={{height:50}} title={`${item.fromStation} - ${item.toStation}`} />
-                                <div className="mt-3"><Tag color="magenta">{`From ${item?.minPrice?.toLocaleString("en-US", { style: "currency", currency: "USD" })}`}</Tag></div>
-                            </Card>
-                        </a>
-                    )
-                })}
-            </div>
+                <div className=" p-4 rounded-xl bg-white">
+                  <Carousel draggable={true} {...settings}  >
+                      {arrPromoteTrip?.map((item, index) => {
+                              return (
+                                <div>
+                                  <h3 >
+                                    <a href={`/search/?from=${item.fromStation}&to=${item.toStation}`} key={index} className="hover:no-underline col-2">
+                                        <Card hoverable style={{ width: '100%' }}
+                                            cover={<img alt="example" style={{ height: 150, objectFit: 'cover' }} src={`${DOMAIN}/Images/PromoteTrip/${item.image}`} />} >
+                                            <Meta title={`${item.fromStation} - ${item.toStation}`} />
+                                            <div className="mt-3"><Tag color="magenta">{`From ${item?.minPrice?.toLocaleString("en-US", { style: "currency", currency: "USD" })}`}</Tag></div>
+                                        </Card>
+                                    </a>
+                                  </h3>
+                                </div>
+                              )
+                        })}
+                  </Carousel>
+                </div>
         </div>
 
     )
