5ae3b4d7d47f31db0c21b74f28ae90fde4ed6ed1
Dustin9x <116355841+Dustin9x@users.noreply.github.com>
2024-01-24 18:06:00 +0700

    update Popular Route

--- frontend/src/components/PopularRoute/PopularRoute.js
+++ frontend/src/components/PopularRoute/PopularRoute.js
@@ -1,6 +1,7 @@
+/* eslint-disable no-lone-blocks */
 import { Card, Carousel, Tag } from "antd";
 import React, { useEffect, useRef } from "react";
-import { SearchOutlined } from '@ant-design/icons';
+import { LeftOutlined, RightOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from "react-redux";
 import { getTripListAction } from "../../redux/actions/TripAction";
 import { DOMAIN } from "../../util/settings/config";
@@ -14,57 +15,46 @@ export default function PopularRoute() {
         dispatch(getPromoteTripListAction())
     }, [])
 
+    let [list, chunkSize] = [arrPromoteTrip, 6];
+    list = [...Array(Math.ceil(list.length / chunkSize))].map(_ => list.splice(0, chunkSize))
+
+    console.log(list);
+
+    console.log('arrPromoteTrip', arrPromoteTrip)
 
     const settings = {
-      autoplay:true,
-      autoplaySpeed:1500,
-      className: "center",
-      slidesToShow: 6,
-      draggable:true,
-      // swipeToSlide: true
+        // autoplay: true,
+        // autoplaySpeed: 1500,
+        // className: "center",
+        // draggable: true,
+        // swipeToSlide: true
     }
 
     return (
         <div >
             <h1 className="text-center text-2xl">Popular bus routes</h1>
-                <div className="pl-4 py-4 rounded-xl bg-white" style={{marginLeft:"-13px", width:"101.8%"}}>
-                  <Carousel nextArrow {...settings}  >
-                      {arrPromoteTrip?.map((item, index) => {
-                              return (
-                                <div>
-                                  <h3 >
-                                    <a href={`/search/?from=${item.fromStation}&to=${item.toStation}`} key={index} className="hover:no-underline col-2">
-                                        <Card hoverable style={{ width: '90%' }}
-                                            cover={<img alt="example" style={{ height: 200, objectFit: 'cover' }} src={`${DOMAIN}/Images/PromoteTrip/${item.image}`} />} >
-                                            <Meta title={`${item.fromStation} - ${item.toStation}`} />
-                                            <div className="mt-3"><Tag color="magenta">{`From ${item?.minPrice?.toLocaleString("en-US", { style: "currency", currency: "USD" })}`}</Tag></div>
-                                        </Card>
-                                    </a>
-                                  </h3>
-                                </div>
-                              )
-                        })}
-                  </Carousel>
-                </div>
-                {/* <h1 className="text-center text-2xl">Popular bus routes</h1>
-            <div className="row p-4 rounded-xl bg-white" >
-            {arrPromoteTrip?.map((item, index) => {
-                    return (
-                        <a href={`/search/?from=${item.fromStation}&to=${item.toStation}`} key={index} className="hover:no-underline col-2">
-                            <Card
-                                hoverable
-                                style={{ width: '100%' }}
-                                cover={<img alt="example" style={{ height: 150, objectFit: 'cover' }} src={`${DOMAIN}/Images/PromoteTrip/${item.image}`} />}
-                            >
-                                <Meta title={`${item.fromStation} - ${item.toStation}`} />
-                                <Meta style={{height:50}} title={`${item.fromStation} - ${item.toStation}`} />
-                                <div className="mt-3"><Tag color="magenta">{`From ${item?.minPrice?.toLocaleString("en-US", { style: "currency", currency: "USD" })}`}</Tag></div>
-                            </Card>
-                        </a>
+            <div className="py-4 rounded-xl bg-white" style={{margin: '0 -15px'}}>
+                <Carousel arrows prevArrow={<LeftOutlined />} nextArrow={<RightOutlined />} draggable={true} style={{height:320}} className="d-block">
+                    {list.map(element => {
+                        return (
+                            <div className="d-flex" >
+                                {element?.map((item, index) => {
+                                    return (
+                                            <a href={`/search/?from=${item.fromStation}&to=${item.toStation}`} key={index} className="hover:no-underline flex justify-center col-2">
+                                                <Card hoverable style={{ width: '90%' }}
+                                                    cover={<img alt="example" style={{ height: 150, objectFit: 'cover' }} src={`${DOMAIN}/Images/PromoteTrip/${item.image}`} />} >
+                                                    <Meta style={{height:50}} title={`${item.fromStation} - ${item.toStation}`} />
+                                                    <div className="mt-3"><Tag color="magenta">{`From ${item?.minPrice?.toLocaleString("en-US", { style: "currency", currency: "USD" })}`}</Tag></div>
+                                                </Card>
+                                            </a>
+                                    )
+                                })}
+                            </div>
+                        )
 
-                    )})}      
-
-                    </div> */}
+                    })}
+                </Carousel>
+            </div>
         </div>
 
     )
